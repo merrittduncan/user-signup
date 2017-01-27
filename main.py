9054d2882964820608fba92100d6f8d425fab4cf
Merritt Duncan <merrittduncan@gmail.com>
2017-01-26 20:00:01 -0600

    added reg expression

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import webapp2
+import re
+
+user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+password_re = re.compile(r"^.{3,20}$")
+email_re = re.compile("^[\S]+@[\S]+.[\S]+$")
+page_header ="""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>User Signup</title>
+    </head>
+    <body>"""
+
+page_footer = """
+    </body>
+    </html>
+    """
 
 def build_page(signup_table):
     username_input = "<tr><td>Username</td><td><input type='text' name='username'></td></tr>"
@@ -36,11 +36,20 @@ def build_page(signup_table):
 
     return header + form
 
+def valid_username(username):
+    return user_re.match(username)
+
+def valid_username(password):
+    return password_re.match(password)
+
+def valid_username(email):
+    return email_re.match(email)
+
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
         content = build_page("")
-        self.response.write(content)
+        self.response.write(page_header + content + page_footer)
 
     def post(self):
         message = self.request.get('message')
